a6f7cf796a6b96cf4f61d04cfa6cf731f39359ad
Alex <90295716+LaLlamaBoyz@users.noreply.github.com>
2023-08-27 14:02:48 -0700

    switched all to java

---
+++ src/main/java/frc/Controls.java
@@ -0,0 +1,267 @@
+package frc;
+import frc.robot.Robot;
+import edu.wpi.first.wpilibj.GenericHID;
+
+public class Controls {
+
+    Robot robot = new Robot();
+
+    public void controls(){
+        if (robot.buttonValueTwo == true){
+            robot.Xbox.setRumble(GenericHID.RumbleType.kRightRumble, 1.0);
+            robot.Xbox.setRumble(GenericHID.RumbleType.kLeftRumble, 1.0);
+            robot.Xbox.setRumble(GenericHID.RumbleType.kRightRumble, 1.0);
+            robot.Xbox.setRumble(GenericHID.RumbleType.kRightRumble, 1.0);
+        }
+        else if (robot.buttonValueTwo == false){
+            robot.Xbox.setRumble(GenericHID.RumbleType.kRightRumble, 0.0);
+            robot.Xbox.setRumble(GenericHID.RumbleType.kLeftRumble, 0.0);
+            robot.Xbox.setRumble(GenericHID.RumbleType.kRightRumble, 0.0);
+            robot.Xbox.setRumble(GenericHID.RumbleType.kRightRumble, 0.0);
+        }
+    
+        // 41 inches
+        // 2988.303 is multiplier by inches
+    
+        robot.mainlimit = 122520.423;
+        // Setting extension limits
+        if (robot.AverageArmEncoderValue <= -15){
+            // number in parameters + (number in parenthese subtracted from AverageArmValue) must equal division factor
+            robot.limitfactor = (robot.AverageArmEncoderValue - 1) / -16;
+            robot.maxextensionlimit = robot.mainlimit / robot.limitfactor;
+        }
+        else{
+            // resetting limits
+            robot.maxextensionlimit = robot.mainlimit;
+        }
+    
+        // Release the piston while the arm is being extended & retracted
+        if (robot.Xbox.getRawButtonPressed(2) || robot.Xbox.getRawButtonPressed(1)){
+            robot.Piston.set(true);
+        }
+        // Stops firing piston while arm is extended & retracted
+        if (robot.Xbox.getRawButtonReleased(1) || robot.Xbox.getRawButtonReleased(2)){
+            robot.Piston.set(false);
+        }
+    
+        if (robot.Xbox.getRawButton(8)){
+            // arm to go
+            if (robot.extensionvalue <= robot.maxextensionlimit){
+            if (robot.extensionvalue <= 75000){
+                robot.Piston.set(true);
+                robot.ExtensionMotorOne.set(0.3);
+                robot.ExtensionMotorTwo.set(0.3);
+            }
+            else{
+                robot.ExtensionMotorOne.set(0);
+                robot.ExtensionMotorTwo.set(0);
+                robot.Piston.set(false);
+            }
+            }
+            else{
+                robot.ExtensionMotorOne.set(0);
+                robot.ExtensionMotorTwo.set(0);
+            }
+    
+            if (robot.AverageArmEncoderValue >= -47){
+            if (robot.AverageArmEncoderValue <= -17.5){
+                robot.ArmUpOne.set(-0.25);
+                robot.ArmUpTwo.set(0.25);
+            }
+            // arm to go down
+            else if (robot.AverageArmEncoderValue >= -15.5){
+                robot.ArmUpOne.set(0.25);
+                robot.ArmUpTwo.set(-0.25);
+            }
+            else{
+                robot.ArmUpOne.set(0);
+                robot.ArmUpTwo.set(0);
+            }
+            }
+            else{
+                robot.ArmUpOne.set(0);
+                robot.ArmUpTwo.set(0);
+            }
+        }
+        // Between ___ & 41 inches, the arm can retract & extend
+        if (robot.Xbox.getRawButton(8)){
+            if (robot.extensionvalue <= robot.maxextensionlimit && robot.extensionvalue >= 0){
+            if (robot.Xbox.getRawButton(1)){
+                robot.ExtensionMotorOne.set(0.3);
+                robot.ExtensionMotorTwo.set(0.3);
+                robot.currentextend = robot.ExtensionMotorOne.getSelectedSensorPosition();
+            }
+            else if (robot.Xbox.getRawButton(2)){
+                robot.ExtensionMotorOne.set(-0.3);
+                robot.ExtensionMotorTwo.set(-0.3);
+            }
+            else{
+                robot.ExtensionMotorOne.set(0);
+                robot.ExtensionMotorTwo.set(0);
+                robot.Piston.set(false);
+                robot.currentextend = robot.ExtensionMotorOne.getSelectedSensorPosition();
+            }
+            }
+            // arm only retract
+            if (robot.extensionvalue >= robot.maxextensionlimit){
+            if (robot.Xbox.getRawButton(2)){
+                robot.ExtensionMotorOne.set(-0.3);
+                robot.ExtensionMotorTwo.set(-0.3);
+                robot.currentextend = robot.ExtensionMotorOne.getSelectedSensorPosition();
+            }
+            else{
+                robot.ExtensionMotorOne.set(-0.2);
+                robot.ExtensionMotorTwo.set(-0.2);
+                robot.Piston.set(true);
+            }
+            }
+            // arm only extend
+            if (robot.extensionvalue <= 0){
+            if (robot.Xbox.getRawButton(1)){
+                robot.ExtensionMotorOne.set(0.3);
+                robot.ExtensionMotorOne.set(0.3);
+                robot.currentextend = robot.ExtensionMotorOne.getSelectedSensorPosition();
+            }
+            else{
+                robot.ExtensionMotorOne.set(0);
+                robot.ExtensionMotorTwo.set(0);
+                robot.currentextend = robot.ExtensionMotorOne.getSelectedSensorPosition();
+            }
+            }
+    
+            if ((robot.AverageArmEncoderValue >= -47) && (robot.AverageArmEncoderValue <= 0)){
+            // autopreset for cube
+            if (robot.Xbox.getRawButton(6)){
+                robot.ArmUpOne.set(-0.25);
+                robot.ArmUpTwo.set(0.25);
+                robot.currentarm = robot.ArmOneEncoder.getPosition();
+            }
+            else if (robot.Xbox.getRawButton(5)){
+                robot.ArmUpOne.set(0.25);
+                robot.ArmUpTwo.set(-0.25);
+                robot.currentarm = robot.ArmOneEncoder.getPosition();
+            }
+            else{
+                robot.ArmUpOne.set(0);
+                robot.ArmUpTwo.set(0);
+                robot.currentarm = robot.ArmOneEncoder.getPosition();
+            }
+            }
+    
+            if (robot.AverageArmEncoderValue <= -47){
+            if (robot.Xbox.getRawButton(6)){
+                robot.ArmUpOne.set(-0.2);
+                robot.ArmUpTwo.set(0.2);
+                robot.currentarm = robot.ArmOneEncoder.getPosition();
+            }
+            else{
+                robot.ArmUpOne.set(0);
+                robot.ArmUpTwo.set(0);
+                robot.currentarm = robot.ArmOneEncoder.getPosition();
+            }
+            }
+            if (robot.AverageArmEncoderValue >= 0){
+            if (robot.Xbox.getRawButton(5)){
+                robot.ArmUpOne.set(0.25);
+                robot.ArmUpTwo.set(-0.25);
+                robot.currentarm = robot.ArmOneEncoder.getPosition();
+            }
+            else{
+                robot.ArmUpOne.set(0);
+                robot.ArmUpTwo.set(0);
+                robot.currentarm = robot.ArmOneEncoder.getPosition();
+            }
+            }
+        }
+        if (robot.Xbox.getRawButtonPressed(3)){
+            robot.bothTake = 2;
+        }
+        if (robot.Xbox.getRawButtonPressed(4)){
+            robot.bothTake = 3;
+        }
+    
+        // Cone intake
+        if (robot.bothTake == 1){
+            if (robot.Xbox.getRawAxis(3) >= 0.1){
+                robot.coneintake = true;
+            }
+            if (robot.coneintake == true){
+                robot.SRX_1.set(1);
+                robot.SRX_2.set(1);
+                robot.SRX_3.set(1);
+                robot.Lights.set(true);
+                robot.Vent1.set(false);
+                robot.Vent2.set(false);
+                robot.Vent3.set(false);
+            }
+            else{
+            robot.SRX_1.set(0);
+            robot.SRX_2.set(0);
+            robot.SRX_3.set(0);
+            robot.Lights.set(false);
+            robot.Vent1.set(true);
+            robot.Vent1.set(true);
+            robot.Vent1.set(true);
+    
+            robot.ClawMotor.set(0);
+            }
+        }
+    
+        // turning it on based on the button pressed
+        if (robot.bothTake == 2){
+            robot.SRX_1.set(1);
+            robot.SRX_2.set(1);
+            robot.SRX_3.set(1);
+            robot.Vent1.set(false);
+            robot.Vent2.set(false);
+            robot.Vent3.set(false);
+            robot.ClawMotor.set(0.3);
+        }
+        // toggling the button off based on the button pressed
+        else if (robot.bothTake == 3){
+            robot.coneintake = false;
+            long now = System.currentTimeMillis();
+            if (now - robot.last <= 3000){
+                robot.ClawMotor.set(-0.15);
+    
+            robot.SRX_1.set(0);
+            robot.SRX_2.set(0);
+            robot.SRX_3.set(0);
+    
+            robot.Vent1.set(true);
+            robot.Vent2.set(true);
+            robot.Vent3.set(true);
+            }
+            else{
+                robot.Vent1.set(false);
+            robot.Vent2.set(false);
+            robot.Vent3.set(false);
+    
+            robot.bothTake = 1;
+            robot.last = System.currentTimeMillis();
+            }
+        }
+    
+        if (robot.JoyStick1.getRawButton(1)){
+            robot.Intake.set(0.5);
+            robot.IntakePiston.set(true);
+        }
+        else if (robot.JoyStick1.getRawButton(3)){
+            robot.Intake.set(-0.4);
+        }
+        else{
+            robot.Intake.set(0);
+            robot.IntakePiston.set(false);
+        }
+    
+        // setting base values for teleop
+        double WheelX = -robot.Wheel.getX();
+        double JoyY = robot.JoyStick1.getY();
+    
+        double lmol = ((WheelX * 0.5) + (0.6 * JoyY));
+        double rmor = ((WheelX * 0.5) - (0.6 * JoyY)) * 0.9368259;
+    
+        robot.FrontLeftMotor.set(lmol);
+        robot.FrontRightMotor.set(rmor);
+        }
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,95 +4,199 @@
 
 package frc.robot;
 
+//import org.opencv.core.Scalar;
+
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.sensors.WPI_PigeonIMU;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.math.filter.SlewRateLimiter;
-import com.revrobotics.CANEncoder;
-import com.revrobotics.REVLibError;
-import com.revrobotics.SparkMaxAbsoluteEncoder;
-import com.revrobotics.SparkMaxAlternateEncoder;
-import com.revrobotics.SparkMaxRelativeEncoder;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-import java.lang.String;
-import com.ctre.phoenix.Util;
-import com.revrobotics.CANPIDController;
-import com.revrobotics.CANSensor;
-import com.revrobotics.CANSparkMax;
-
-import edu.wpi.first.math.ComputerVisionUtil;
-import edu.wpi.first.math.util.Units; 
-import edu.wpi.first.wpilibj.PowerDistribution;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.AnalogGyro;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.BuiltInAccelerometer;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.PneumaticsControlModule;
-import edu.wpi.first.networktables.NetworkTableValue;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.PneumaticsBase;
-import edu.wpi.first.wpilibj.CompressorConfigType;
-import edu.wpi.first.wpilibj.util.WPILibVersion;
-import java.lang.Math;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import edu.wpi.first.cameraserver.CameraServer;
-import java.util.Timer;
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.AutoF;
+import frc.Controls;
 
 public class Robot extends TimedRobot {
+  private AutoF autoF = new AutoF();
+  private Controls controls = new Controls();
   private Command m_autonomousCommand;
   private RobotContainer m_robotContainer;
 
   //Joysticks
-  Joystick xbox = new Joystick(2), joystick = new Joystick(2), wheel = new Joystick(2);
+  public Joystick Xbox = new Joystick(2), JoyStick1 = new Joystick(2), Wheel = new Joystick(2);
 
   //Drivetrain motors
-  CANSparkMax FrontLeftMotor = new CANSparkMax(1, MotorType.kBrushless);
-  CANSparkMax MiddleLeftMotor = new CANSparkMax(3, MotorType.kBrushless);
-  CANSparkMax BackLeftMotor = new CANSparkMax(5, MotorType.kBrushless);
-  CANSparkMax FrontRightMotor = new CANSparkMax(2, MotorType.kBrushless);
-  CANSparkMax MiddleRightMotor = new CANSparkMax(4, MotorType.kBrushless);
-  CANSparkMax BackRightMotor = new CANSparkMax(6, MotorType.kBrushless);
+  public CANSparkMax FrontLeftMotor = new CANSparkMax(1, MotorType.kBrushless);
+  public CANSparkMax MiddleLeftMotor = new CANSparkMax(3, MotorType.kBrushless);
+  public CANSparkMax BackLeftMotor = new CANSparkMax(5, MotorType.kBrushless);
+  public CANSparkMax FrontRightMotor = new CANSparkMax(2, MotorType.kBrushless);
+  public CANSparkMax MiddleRightMotor = new CANSparkMax(4, MotorType.kBrushless);
+  public CANSparkMax BackRightMotor = new CANSparkMax(6, MotorType.kBrushless);
 
   //Intake motor
-  CANSparkMax IntakeMotor = new CANSparkMax(25, MotorType.kBrushless);
+  public CANSparkMax IntakeMotor = new CANSparkMax(25, MotorType.kBrushless);
 
   //DriveTrain encoder
-  
+  public RelativeEncoder LeftEncoder = FrontLeftMotor.getEncoder();
+  public RelativeEncoder RightEncoder = FrontRightMotor.getEncoder();
+
 
   //Arm Motors
-  CANSparkMax ArmUpOne = new CANSparkMax(7, MotorType.kBrushless);
-  CANSparkMax ArmUpTwo = new CANSparkMax(8, MotorType.kBrushless);
+  public CANSparkMax ArmUpOne = new CANSparkMax(7, MotorType.kBrushless);
+  public CANSparkMax ArmUpTwo = new CANSparkMax(8, MotorType.kBrushless);
 
-  WPI_TalonFX ClawMotor = new WPI_TalonFX(9);
-  WPI_TalonFX ExtensionMotorOne = new WPI_TalonFX(10);
-  WPI_TalonFX ExtensionMotorTwo = new WPI_TalonFX(11);
-  WPI_TalonFX Intake = new WPI_TalonFX(25);
+  public WPI_TalonFX ClawMotor = new WPI_TalonFX(9);
+  public WPI_TalonFX ExtensionMotorOne = new WPI_TalonFX(10);
+  public WPI_TalonFX ExtensionMotorTwo = new WPI_TalonFX(11);
+  public WPI_TalonFX Intake = new WPI_TalonFX(25);
+
 
   //Arm encoders
-  ArmOneEncoder = ArmUpOne.getEncoder(), ArmTwoEncoder = ArmUpTwo.getEncoder();
+  public RelativeEncoder ArmOneEncoder = ArmUpOne.getEncoder(), ArmTwoEncoder = ArmUpTwo.getEncoder();
   
+  //Solenoids & compresser
+  public Compressor pcmCompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
+  public Solenoid Piston = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
+  public Solenoid IntakePiston = new Solenoid(PneumaticsModuleType.CTREPCM, 1);
+  public Solenoid Lights = new Solenoid(PneumaticsModuleType.CTREPCM, 3);
 
-  
+  public Solenoid Vent1 = new Solenoid(PneumaticsModuleType.CTREPCM, 5);
+  public Solenoid Vent2 = new Solenoid(PneumaticsModuleType.CTREPCM, 6);
+  public Solenoid Vent3 = new Solenoid(PneumaticsModuleType.CTREPCM, 7);
+
+  //robot variables
+  public double LeftEncoderValue;
+  public double RightEncoderValue;
+  public double ArmOneEncoderValue;
+  public double ArmTwoEncoderValue;
+  public double speed;
+  public double AverageEncoderValue;
+  public double AverageArmEncoderValue;
+
+  public boolean autoStart = false;
+  public double maxextensionlimit;
+  public double mainlimit;
+  public double maxanglelimit = 4;
+  public boolean subpreset = false;
+  public boolean pistonenable = false;
+  // set amp limit
+  public boolean enable = true;
+  public boolean brakemode = false;
+  public double currentLimit = 60;
+  public double triggerThresholdCurrent = 60;
+  public double triggerThresholdTime = .1;
+
+  public double currentarm;
+  public double currentextend;
+  public double limitfactor;
+  // Gyro
+  public WPI_PigeonIMU gyro = new WPI_PigeonIMU(12);
+
+  // compressors
+  public WPI_TalonSRX SRX_1 = new WPI_TalonSRX(15);
+  public WPI_TalonSRX SRX_2 = new WPI_TalonSRX(16);
+  public WPI_TalonSRX SRX_3 = new WPI_TalonSRX(17);
+
+  // Auto Variables
+  public int autoStep = 1;
+  public double extensionvalue;
+  public String autoChooser = SmartDashboard.getString("DB/String 2", "myDefaultData");;
+
+  // Timer
+  public boolean gyroResetted = false;
+  public long last = System.currentTimeMillis();
+
+  // dashboard variables
+  public boolean buttonValue;
+  public boolean buttonValueTwo;
+  public boolean buttonValueThree;
+  public boolean buttonValueFour;
+  public double k_rampTimeSeconds = 0.25;
+
+  public boolean timerStarted = false;
+
+  public double highscorearm = -16.93;
+  public double highscoreextend = 72000;
+  public double mediumscorearm;
+  public double mediumscoreextend;
+
+  public double goalextend;
+
+  // PID Aspects
+  public double YAW;
+  public double ROLL;
+  public double PITCH;
+  public double SPEED;
+  public double driveLimit;
+
+  public boolean coneintake;
+
+  // Intake Outake Variable
+  public int bothTake = 1;
 
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
+    IntakePiston.set(true);
+    gyro.reset();
+    Piston.set(false);
+
+    gyro.calibrate();
+    pcmCompressor.disable();
+    ExtensionMotorOne.setSelectedSensorPosition(0);
+    ExtensionMotorTwo.setSelectedSensorPosition(0);
+
+    // Reset encoders
+    RightEncoder.setPosition(0);
+    LeftEncoder.setPosition(0);
+    ArmOneEncoder.setPosition(0);
+    ArmTwoEncoder.setPosition(0);
+
+    // camera
+    CameraServer.startAutomaticCapture();
+
+    // Motor supply limits
+    SupplyCurrentLimitConfiguration current_limit_config = new SupplyCurrentLimitConfiguration(enable, currentLimit, triggerThresholdCurrent, triggerThresholdTime);
+    SupplyCurrentLimitConfiguration current_claw_config = new SupplyCurrentLimitConfiguration(enable, 30, 30, triggerThresholdTime);
+    ExtensionMotorOne.configSupplyCurrentLimit(current_limit_config);
+    ExtensionMotorTwo.configSupplyCurrentLimit(current_limit_config);
+    ClawMotor.configSupplyCurrentLimit(current_claw_config);
+
+    // Follow front motors
+    MiddleLeftMotor.follow(FrontLeftMotor);
+    BackLeftMotor.follow(FrontLeftMotor);
+    MiddleRightMotor.follow(FrontRightMotor);
+    BackRightMotor.follow(FrontRightMotor);
+
+    // Neo motor current limit
+    FrontLeftMotor.setSmartCurrentLimit(40);
+    MiddleLeftMotor.setSmartCurrentLimit(40);
+    BackLeftMotor.setSmartCurrentLimit(40);
+    FrontRightMotor.setSmartCurrentLimit(40);
+    MiddleRightMotor.setSmartCurrentLimit(40);
+    BackRightMotor.setSmartCurrentLimit(40);
+
+    // Setting Idle Mode to brake (neo motors)
+    FrontLeftMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    MiddleLeftMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    BackLeftMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    FrontRightMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    MiddleRightMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    BackRightMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+
+    ArmUpOne.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    ArmUpTwo.setIdleMode(CANSparkMax.IdleMode.kBrake);
+
   }
   
   @Override
@@ -109,24 +213,139 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
+    // Enabling the PCM compressor/Pnuematically controlled
+    pcmCompressor.enableDigital();
+    ArmOneEncoder.setPosition(0);
+    ArmTwoEncoder.setPosition(0);
+    ExtensionMotorOne.setSelectedSensorPosition(0);
+    ExtensionMotorTwo.setSelectedSensorPosition(0);
+    gyro.reset();
+    autoStep = 1;
+    RightEncoder.setPosition(0);
+    LeftEncoder.setPosition(0);
+    ExtensionMotorOne.setSelectedSensorPosition(0);
+    ExtensionMotorTwo.setSelectedSensorPosition(0);
+
+    gyro.setYaw(0);
+
+    CameraServer.startAutomaticCapture();
+
+    timerStarted = false;
   }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+    // Enabling the PCM compressor
+    Piston.set(false);
+    YAW = gyro.getYaw();
+    PITCH = gyro.getPitch();
+    ROLL = gyro.getRoll() - 2;
+    SmartDashboard.putString("DB/String 8", String.valueOf(ROLL));
+    SmartDashboard.putString("DB/String 9", String.valueOf(YAW));
+    SmartDashboard.putString("DB/String 3", String.valueOf(maxextensionlimit));
+    SmartDashboard.putString("DB/String 8", String.valueOf(ROLL));
+    autoChooser = SmartDashboard.getString("DB/String 2", "myDefaultData");
+    if (autoChooser == "1"){
+      SmartDashboard.putString("DB/String 3", "AutoOne");
+    }
+    else if (autoChooser == "2"){
+      SmartDashboard.putString("DB/String 3", "AutoTwo");
+    }
+    else if (autoChooser == "3"){
+      SmartDashboard.putString("DB/String 3", "AutoThree");
+    }
+
+    AverageEncoderValue = (LeftEncoderValue + RightEncoderValue) / 2;
+
+    LeftEncoderValue = -LeftEncoder.getPosition();
+    RightEncoderValue = RightEncoder.getPosition();
+
+    SmartDashboard.putString("DB/String 7", ("Left: " + String.valueOf(AverageEncoderValue)));
+    SmartDashboard.putString("DB/String 0", ("Left: " + String.valueOf(LeftEncoderValue)));
+    SmartDashboard.putString("DB/String 1", ("Right: " + String.valueOf(RightEncoderValue)));
+
+    AverageArmEncoderValue = (ArmTwoEncoderValue + ArmOneEncoderValue) / 2;
+    ArmOneEncoderValue = -ArmOneEncoder.getPosition();
+    ArmTwoEncoderValue = ArmTwoEncoder.getPosition();
+
+    extensionvalue = ExtensionMotorOne.getSelectedSensorPosition();
+    autoF.autoF1();
+    autoF.autoF2();
+    autoF.autoF3();
+    autoF.autoF4();
+    autoF.autoF5();
+    autoF.autoF6();
+    autoF.autoF7();
+    autoF.autoF8();
+    autoF.autoF9();
+  }
 
   @Override
   public void teleopInit() {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+    timerStarted = false;
+    gyro.setYaw(0);
+    gyro.calibrate();
+    Piston.set(false);
+    IntakePiston.set(false);
+    Vent1.set(false);
+    Vent2.set(false);
+    Vent3.set(false);
+    SRX_1.set(0);
+    SRX_2.set(0);
+    SRX_3.set(0);
+
+    ClawMotor.set(0);
+    ArmOneEncoder.setPosition(0);
+    ArmTwoEncoder.setPosition(0);
+    ExtensionMotorOne.setSelectedSensorPosition(0);
+    ExtensionMotorTwo.setSelectedSensorPosition(0);
+
+    gyro.reset();
+    // Resetting sensor positions using buttons on the dashboard
+
+    RightEncoder.setPosition(0);
+    LeftEncoder.setPosition(0);
+    gyro.reset();
+    last = System.currentTimeMillis();
   }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+
+    buttonValueTwo = SmartDashboard.getBoolean("DB/Button 1", false);
+
+    SmartDashboard.putString("DB/String 0", "LD " + String.valueOf(LeftEncoderValue));
+    SmartDashboard.putString("DB/String 1", "RD " + String.valueOf(RightEncoderValue));
+    SmartDashboard.putString("DB/String 3", "EL " + String.valueOf(maxextensionlimit));
+    SmartDashboard.putString("DB/String 4", "EV " + String.valueOf(extensionvalue));
+    SmartDashboard.putString("DB/String 5", "AOV " + String.valueOf(ArmOneEncoderValue));
+    SmartDashboard.putString("DB/String 6", "ATV " + String.valueOf(ArmTwoEncoderValue));
+    SmartDashboard.putString("DB/String 7", "AVDV " + String.valueOf(AverageEncoderValue));
+    SmartDashboard.putString("DB/String 8", "ROLL " + ((String.valueOf(ROLL))));
+    SmartDashboard.putString("DB/String 9", "YAW " + ((String.valueOf(YAW))));
+
+    pcmCompressor.enableDigital();
+    extensionvalue = ExtensionMotorOne.getSelectedSensorPosition();
+
+    ROLL = gyro.getRoll() - 2;
+    YAW = gyro.getYaw();
+
+    LeftEncoderValue = -LeftEncoder.getPosition();
+    RightEncoderValue = RightEncoder.getPosition();
+    AverageEncoderValue = (LeftEncoderValue + RightEncoderValue) / 2;
+
+    ArmOneEncoderValue = -ArmOneEncoder.getPosition();
+    ArmTwoEncoderValue = ArmTwoEncoder.getPosition();
+    AverageArmEncoderValue = (ArmTwoEncoderValue + ArmOneEncoderValue) / 2;
+
+    controls.controls();
+  }
 
   @Override
   public void testInit() {
